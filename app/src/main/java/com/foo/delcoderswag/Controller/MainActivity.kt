cbb43f4b15def4cb42e21668977304b1fe3c41e2
davidchhetri <david_chhetri@yahoo.com>
2021-12-23 10:14:31 -0500

    initial commit

---
+++ app/src/main/java/com/foo/delcoderswag/Controller/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.foo.delcoderswag.Controller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.foo.delcoderswag.R
+
+
+/*
+- create recycleViewAdapter
+- add onClick event
+- add ProductsActivity
+*/
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/foo/delcoderswag/Model/Product.kt
@@ -0,0 +1,6 @@
+package com.foo.delcoderswag.Model
+
+/**
+ * Created by David Chhetri on 22,November,2021
+ */
+class Product(var title: String, var price: String, var image: String)
